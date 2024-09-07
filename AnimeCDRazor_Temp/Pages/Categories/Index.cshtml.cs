907859b8a326282ea9a51ee692adf1af73aa1104
Le Trong Phuc <ltphuc1802@gmail.com>
2024-09-07 23:06:50 +0700

    Create Category Post Handler

---
+++ AnimeCDRazor_Temp/Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using AnimeCDRazor_Temp.Data;
+using AnimeCDRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AnimeCDRazor_Temp.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        // gõ prop + tab để tạo property
+        public List<Category> CategoryList { get; set; }
+
+        // gõ ctor + tab để tạo constructor
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet()
+        {
+            CategoryList = _db.Categories.ToList(); // sử dụng Entity Framework Core để truy xuất dữ liệu từ bảng Categories
+        }
+    }
+}
